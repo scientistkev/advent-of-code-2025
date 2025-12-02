dc91c3a8f009d7bd367e6e53662321d9271d7e92
Kevin McPherson <16882088+scientistkev@users.noreply.github.com>
2025-12-01 19:55:02 -0500

    Add initial implementation of lock movement logic

---
+++ day_1.py
@@ -0,0 +1,5 @@
+# 1. Lock starts at position p
+# 2. The lock can move a distance d
+# 3. The lock can move in two directions: left (L) and right (R), -d or +d
+# 3a. If there were no wraparound, p+d is the new position when moving right
+# 3b. If there were no wraparound, p-d is the new position when moving left
