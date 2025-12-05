16d40bb6e21e977989f821e1e176e46bb5234a4b
Kevin McPherson <16882088+scientistkev@users.noreply.github.com>
2025-12-04 23:27:30 -0500

    Add implementation for removing accessible rolls of paper in Day 5 challenge

--- challenges/day_5.py
+++ challenges/day_5.py
@@ -59,9 +59,178 @@ def count_accessible_rolls(grid):
 
 
 if __name__ == "__main__":
-    with open("challenges/day_5_input.txt") as f:
+    with open("data/day_5_input.txt") as f:
         input_data = f.read()
 
     grid = parse_input(input_data)
     result = count_accessible_rolls(grid)
     print(f"Number of accessible rolls of paper: {result}")
+
+
+# Now, the Elves just need help accessing as much of the paper as they can.
+
+# Once a roll of paper can be accessed by a forklift, it can be removed. Once a roll of paper is removed, the forklifts might be able to access more rolls of paper, which they might also be able to remove. How many total rolls of paper could the Elves remove if they keep repeating this process?
+
+# Starting with the same example as above, here is one way you could remove as many rolls of paper as possible, using highlighted @ to indicate that a roll of paper is about to be removed, and using x to indicate that a roll of paper was just removed:
+
+# Initial state:
+# ..@@.@@@@.
+# @@@.@.@.@@
+# @@@@@.@.@@
+# @.@@@@..@.
+# @@.@@@@.@@
+# .@@@@@@@.@
+# .@.@.@.@@@
+# @.@@@.@@@@
+# .@@@@@@@@.
+# @.@.@@@.@.
+
+# Remove 13 rolls of paper:
+# ..xx.xx@x.
+# x@@.@.@.@@
+# @@@@@.x.@@
+# @.@@@@..@.
+# x@.@@@@.@x
+# .@@@@@@@.@
+# .@.@.@.@@@
+# x.@@@.@@@@
+# .@@@@@@@@.
+# x.x.@@@.x.
+
+# Remove 12 rolls of paper:
+# .......x..
+# .@@.x.x.@x
+# x@@@@...@@
+# x.@@@@..x.
+# .@.@@@@.x.
+# .x@@@@@@.x
+# .x.@.@.@@@
+# ..@@@.@@@@
+# .x@@@@@@@.
+# ....@@@...
+
+# Remove 7 rolls of paper:
+# ..........
+# .x@.....x.
+# .@@@@...xx
+# ..@@@@....
+# .x.@@@@...
+# ..@@@@@@..
+# ...@.@.@@x
+# ..@@@.@@@@
+# ..x@@@@@@.
+# ....@@@...
+
+# Remove 5 rolls of paper:
+# ..........
+# ..x.......
+# .x@@@.....
+# ..@@@@....
+# ...@@@@...
+# ..x@@@@@..
+# ...@.@.@@.
+# ..x@@.@@@x
+# ...@@@@@@.
+# ....@@@...
+
+# Remove 2 rolls of paper:
+# ..........
+# ..........
+# ..x@@.....
+# ..@@@@....
+# ...@@@@...
+# ...@@@@@..
+# ...@.@.@@.
+# ...@@.@@@.
+# ...@@@@@x.
+# ....@@@...
+
+# Remove 1 roll of paper:
+# ..........
+# ..........
+# ...@@.....
+# ..x@@@....
+# ...@@@@...
+# ...@@@@@..
+# ...@.@.@@.
+# ...@@.@@@.
+# ...@@@@@..
+# ....@@@...
+
+# Remove 1 roll of paper:
+# ..........
+# ..........
+# ...x@.....
+# ...@@@....
+# ...@@@@...
+# ...@@@@@..
+# ...@.@.@@.
+# ...@@.@@@.
+# ...@@@@@..
+# ....@@@...
+
+# Remove 1 roll of paper:
+# ..........
+# ..........
+# ....x.....
+# ...@@@....
+# ...@@@@...
+# ...@@@@@..
+# ...@.@.@@.
+# ...@@.@@@.
+# ...@@@@@..
+# ....@@@...
+
+# Remove 1 roll of paper:
+# ..........
+# ..........
+# ..........
+# ...x@@....
+# ...@@@@...
+# ...@@@@@..
+# ...@.@.@@.
+# ...@@.@@@.
+# ...@@@@@..
+# ....@@@...
+
+# Stop once no more rolls of paper are accessible by a forklift. In this example, a total of 43 rolls of paper can be removed.
+
+
+def remove_accessible_rolls(grid):
+    rows = len(grid)
+    cols = len(grid[0])
+    total_removed = 0
+
+    directions = [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)]
+
+    while True:
+        to_remove = []
+        for r in range(rows):
+            for c in range(cols):
+                if grid[r][c] == "@":
+                    adjacent_rolls = 0
+                    for dr, dc in directions:
+                        nr, nc = r + dr, c + dc
+                        if 0 <= nr < rows and 0 <= nc < cols:
+                            if grid[nr][nc] == "@":
+                                adjacent_rolls += 1
+                    if adjacent_rolls < 4:
+                        to_remove.append((r, c))
+
+        if not to_remove:
+            break
+
+        for r, c in to_remove:
+            grid[r][c] = "."
+        total_removed += len(to_remove)
+
+    return total_removed
+
+
+if __name__ == "__main__":
+    with open("data/day_5_input.txt") as f:
+        input_data = f.read()
+
+    grid = parse_input(input_data)
+    result = remove_accessible_rolls(grid)
+    print(f"Total rolls of paper that can be removed: {result}")
